a844d17ea0ca5664549c1f6cd3f57587e20925bf
Mohammad Nakhjiri <mohammaddev02@gmail.com>
2022-06-27 19:38:07 +0430

    Register finished User handling finished

---
+++ src/database/User.java
@@ -0,0 +1,77 @@
+package database;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class User implements Serializable {
+    public static transient String usersPath = "Users.txt";
+    public String name;
+    public int phoneNumber;
+    public String mail;
+    public String pass;
+    public User(String name, int phoneNumber, String mail, String pass) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.mail = mail;
+        this.pass = pass;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return phoneNumber == user.phoneNumber && Objects.equals(name, user.name) && Objects.equals(mail, user.mail) && Objects.equals(pass, user.pass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber, mail, pass);
+    }
+
+    public static boolean addUser(String userJson){
+        Gson gson = new Gson();
+        User user = gson.fromJson(userJson , User.class);
+        User[] users = getUsers();
+        for(User fileUser : users){
+            if(fileUser.phoneNumber == user.phoneNumber){
+                return false;
+            }
+        }
+        User[] result = Arrays.copyOf(users , users.length + 1);
+        result[result.length - 1] = user;
+        try(FileOutputStream fileOutputStream = new FileOutputStream(usersPath);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)
+        ){
+            objectOutputStream.writeObject(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public static User[] getUsers(){
+        ArrayList<User> result = new ArrayList<>();
+        try{
+
+            if(!Files.exists(Path.of(usersPath))){
+                Files.createFile(Path.of(usersPath));
+                return result.toArray(new User[0]);
+            }
+            try(FileInputStream fileInputStream = new FileInputStream(usersPath);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)){
+                if(objectInputStream.readObject() instanceof User[]){
+                    return  (User[]) objectInputStream.readObject();
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toArray(new User[0]);
+    }
+}
--- src/network/RegisterHandler.java
+++ src/network/RegisterHandler.java
@@ -1,9 +1,42 @@
 package network;
 
+import database.User;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class RegisterHandler implements  Runnable{
     final int port = 2525;
     @Override
     public void run() {
+        ServerSocket serverSocket;
+        try{
+            serverSocket = new ServerSocket(port);
+            while (true){
+                try(Socket socket = serverSocket.accept();
+                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                ){
+                    StringBuilder command = new StringBuilder();
+                    int c = dataInputStream.read();
+                    while (c != -1 && c != 10) {
+                        command.append((char)c);
+                        c = dataInputStream.read();
+                    }
+                    boolean result = User.addUser(command.toString());
+                    if(result){
+                        dataOutputStream.writeBytes("registered");
+                    }else{
+                        dataOutputStream.writeBytes("unregistered");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
